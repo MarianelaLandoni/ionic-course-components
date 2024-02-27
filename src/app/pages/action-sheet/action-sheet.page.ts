37c98e312fc5f018fa62bb3d49629e3771ee6bfe
Marianela Landoni <mlandoni@dnestagency.com>
2024-02-27 16:48:39 +0100

    add action sheet page

---
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-action-sheet',
+  templateUrl: './action-sheet.page.html',
+  styleUrls: ['./action-sheet.page.scss'],
+})
+export class ActionSheetPage {
+
+  constructor() { }
+
+}
